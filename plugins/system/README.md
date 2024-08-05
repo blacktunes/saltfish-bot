196ba53057d81cea3896fc7d24d019ebef22eb91
blacktunes <blacktunes@foxmail.com>
2024-08-05 19:51:39 +0800

    feat(system): 完善插件功能

--- plugins/system/README.md
+++ plugins/system/README.md
@@ -1,20 +1,23 @@
 # @saltfish-bot/plugin-system
 
-**咸鱼BOT所使用的插件，可以获取运行咸鱼BOT的设备状态**
-
-> 默认指令为 **#system**
+**咸鱼BOT所使用的插件，可以获取运行咸鱼BOT的设备状态和BOT的状态以及指令说明**
 
 ```ts
 import App from 'saltfish-bot'
-import system from '@saltfish-bot/plugin-system'
+import Sys from '@saltfish-bot/plugin-system'
 
 const app = new App()
 app
-  .plugin(Sys, {
-    // 指令
-    command: '#system',
-    // 是否需要管理员
-    admin: true
-  })
+  .plugin(Sys)
   .start()
 ```
+
+### 指令
+- 查看服务器状态
+  - #sys
+  - #服务器状态
+- 查看BOT状态
+  - #bot
+  - #BOT状态
+- 查看所有可用指令
+  - #help
--- plugins/system/src/index.ts
+++ plugins/system/src/index.ts
@@ -1,46 +1,185 @@
-import { htmlToImage } from '@saltfish-bot/puppeteer'
+import { BotPlugin } from 'saltfish-bot'
 import { cpu, mem } from 'node-os-utils'
-import { BotPlugin, PluginConfig, secondsFormat } from 'saltfish-bot'
+import si = require('systeminformation')
+import { htmlToImage } from '@saltfish-bot/puppeteer'
 import path = require('path')
 
-interface Config {
-  // 触发命令
-  command?: string
-  // 是否需要管理员
-  admin?: boolean
-}
-
 export default class System extends BotPlugin {
-  name = 'system'
-
-  config: PluginConfig<Config> = {
-    command: '#system',
-    admin: true
-  }
+  static name = 'system'
 
   init = () => {
-    this.Command.command(this.config.command)
-      .admin(this.config.admin)
-      .desc('查询服务器状态')
+    this.Command.command('#help')
+      .desc('查看所有可用指令')
       .action('group', async (e) => {
-        const cpuUsage = await cpu.usage()
-        const memInfo = await mem.info()
-        const img = await htmlToImage(`file://${path.join(__dirname, 'system.html')}`, {
-          name: this.Bot.Data.name,
-          time: secondsFormat(Math.floor(process.uptime())),
-          plugin: this.Bot.Plugin.list.length,
-          command: this.Bot.Command.list.length,
-          listener: this.Bot.Conn.getEventNum(),
-          message: this.Bot.Conn.getMessageNum(),
-          cpu: `${cpuUsage}%`,
-          mem: [
-            `${((memInfo.usedMemMb / memInfo.totalMemMb) * 100.0).toFixed(2)}%`,
-            `${((process.memoryUsage().rss / 1024 / 1024 / memInfo.totalMemMb) * 100.0).toFixed(
-              2
-            )}%`
-          ]
+        setTimeout(async () => {
+          const commands: {
+            [name: string]: {
+              name: string[]
+              reg?: string
+              admin?: boolean
+              desc?: string
+            }[]
+          } = {}
+          const _bot: {
+            name: string[]
+            reg?: string
+            admin?: boolean
+            desc?: string
+          }[] = []
+          this.Bot.Command.list.forEach((item) => {
+            if (item.group) {
+              const plugin = this.Bot.Plugin.getPlugin<BotPlugin>(item.group)
+              const blacklist = plugin?.blacklist || {}
+              const whitelist = plugin?.whitelist || {}
+
+              if (
+                !(
+                  (blacklist.group && blacklist.group.has(e.group_id)) ||
+                  (blacklist.user && blacklist.user.has(e.user_id)) ||
+                  (whitelist.group && !whitelist.group.has(e.group_id)) ||
+                  (whitelist.user && !whitelist.user.has(e.user_id))
+                ) &&
+                !(
+                  (item.blacklist.group && item.blacklist.group.has(e.group_id)) ||
+                  (item.blacklist.user && item.blacklist.user.has(e.user_id)) ||
+                  (item.whitelist.group && !item.whitelist.group.has(e.group_id)) ||
+                  (item.whitelist.user && !item.whitelist.user.has(e.user_id))
+                ) &&
+                ((item.admin && this.Bot.Admin.isAdmin(e.user_id)) || !item.admin)
+              ) {
+                if (!(item.group in commands)) {
+                  commands[item.group] = []
+                }
+                commands[item.group].push({
+                  name: item.comm,
+                  reg: item.reg ? String(item.reg) : undefined,
+                  admin: item.admin,
+                  desc: item.desc
+                })
+              }
+            } else {
+              if (
+                !(
+                  (item.blacklist.group && item.blacklist.group.has(e.group_id)) ||
+                  (item.blacklist.user && item.blacklist.user.has(e.user_id)) ||
+                  (item.whitelist.group && !item.whitelist.group.has(e.group_id)) ||
+                  (item.whitelist.user && !item.whitelist.user.has(e.user_id))
+                ) &&
+                ((item.admin && this.Bot.Admin.isAdmin(e.user_id)) || !item.admin)
+              ) {
+                _bot.push({
+                  name: item.comm,
+                  reg: item.reg ? String(item.reg) : undefined,
+                  admin: item.admin,
+                  desc: item.desc
+                })
+              }
+            }
+          })
+
+          if (_bot.length > 0) {
+            commands._bot = _bot
+          }
+
+          const img = await htmlToImage(
+            `file://${path.join(__dirname, 'help/index.html')}`,
+            commands
+          )
+
+          this.Bot.Api.sendGroupMsg(e.group_id, this.Bot.CQCode.image(img))
         })
-        this.Bot.Api.sendGroupMsg(e.group_id, this.Bot.CQCode.image(img))
+
+        return true
+      })
+
+    this.Command.command('#bot')
+      .alias('#BOT状态')
+      .admin()
+      .desc('查看BOT状态')
+      .action('group', (e) => {
+        setTimeout(async () => {
+          const commands: {
+            [name: string]: {
+              name: string
+              reg?: string
+              admin?: boolean
+            }[]
+          } = {}
+          const _bot: {
+            name: string
+            reg?: string
+            admin?: boolean
+          }[] = []
+          this.Bot.Command.list.forEach((item) => {
+            if (item.group) {
+              if (!(item.group in commands)) {
+                commands[item.group] = []
+              }
+              commands[item.group].push({
+                name: item.comm[0],
+                reg: item.reg ? String(item.reg) : undefined,
+                admin: item.admin
+              })
+            } else {
+              _bot.push({
+                name: item.comm[0],
+                reg: item.reg ? String(item.reg) : undefined,
+                admin: item.admin
+              })
+            }
+          })
+
+          if (_bot.length > 0) {
+            commands._bot = _bot
+          }
+
+          const img = await htmlToImage(`file://${path.join(__dirname, 'bot/index.html')}`, {
+            name: this.Bot.Data.name,
+            time: process.uptime(),
+            memory: (process.memoryUsage().rss / 1024 / 1024).toFixed(1),
+            message: this.Bot.Conn.getMessageNum(),
+            plugins: this.Bot.Plugin.list.map((item) => item.name),
+            commands,
+            events: this.Bot.Conn.getEventInfo()
+          })
+
+          this.Bot.Api.sendGroupMsg(e.group_id, this.Bot.CQCode.image(img))
+        }, 0)
+
+        return true
+      })
+
+    this.Command.command('#sys')
+      .alias('#服务器状态')
+      .admin()
+      .desc('查看服务器状态')
+      .action('group', (e) => {
+        setTimeout(async () => {
+          const cpuUsage = await cpu.usage()
+          const memInfo = await mem.info()
+          await si.networkStats()
+          const network = await si.networkStats()
+          const processes = (await si.processes()).list
+            .sort((a, b) => b.memRss - a.memRss)
+            .slice(0, 5)
+
+          const img = await htmlToImage(`file://${path.join(__dirname, 'sys/index.html')}`, {
+            cpu: cpuUsage,
+            men: {
+              total: memInfo.totalMemMb,
+              used: memInfo.usedMemMb,
+              percentage: memInfo.usedMemPercentage
+            },
+            network: {
+              up: network[0].tx_sec,
+              down: network[0].rx_sec
+            },
+            processes
+          })
+
+          this.Bot.Api.sendGroupMsg(e.group_id, this.Bot.CQCode.image(img))
+        }, 0)
+
         return true
       })
   }
